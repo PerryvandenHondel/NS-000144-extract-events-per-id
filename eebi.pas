8aef77c4056d932454b8d6fc45fe05667e6e18bc
Perry van den Hondel <perry.vandenhondel@gmail.com>
2015-10-02 15:49:53 +0200

    Write only a header line with a new export CSV file

--- eebi.pas
+++ eebi.pas
@@ -3,6 +3,16 @@
 //
 //	R:\GitRepos\NS-000144-extract-events-by-id\NS00DC012\08db66346664a0b5.lpr					
 //
+//	PROCEDURES AND FUNCTIONS
+//		function ExtractUniqueIdFromPath(p: string): string;
+//		function FixLine(strLine: AnsiString): AnsiString;
+//		function ReadSettingKey(section: string; key: string): string;
+//		procedure ProcessLine(const e: integer; uid: string; lineNumber: integer; l: AnsiString);
+//		procedure ProcessLprFile(const e: string; const p: string);
+//		procedure ProgDone();
+//		procedure ProgInit();
+//		procedure ProgRun();
+//		procedure ProgUsage();
 //
 
 
@@ -23,68 +33,73 @@ uses
 	UTextSeparated,
 	USupportLibrary;
 
-
-type
-	// Type definition of the Event Records
-	REventFile = record
-		EventId: integer;			// What's the current
-		FilePointer: TextFile;		// File pointer to the file.
-		Header: AnsiString;			// Store the header line in this.
-		HeaderCount: integer;		// Number of items in the header;
-		count: integer;				// Count the number of records
-		Path: string;				// Path to the export file. D:\folder\events-XXXX.tsv.
-	end;
-	AEventFile = array of REventFile;
-
-
+	
 const
-	TAB = 				#9;
-	SEP = 				'|';
-	STEP_MOD = 			127;
+	STEP_MOD = 			257;
+	CONF_NAME = 		'eebi.conf';
 
 
 var
-	//gintLineCount: integer;
-	//gtfTsv: TextFile;
-	//gstrEventId: string;
-	arrEventFile: AEventFile;
 	csv: CTextFile;
-	//gblnAppend: boolean;
 
+	
 
-procedure SafeCopy(fromFile, toFile : string);
-type 
-	bufferType = array [1..65535] of char;
-	bufferTypePtr = ^bufferType;  { Use the heap }
-var 
-	bufferPtr : bufferTypePtr;     { for the buffer }
-	f1, f2 : file;
-	bufferSize, readCount, writeCount : word;
-	fmSave : byte;              { To store the filemode }
+function ReadSettingKey(section: string; key: string): string;
+//
+//	Read a Key from a section from a config (.conf) file.
+//
+//	[Section]
+//	Key1=10
+//	Key2=Something
+//
+//	Usage:
+//		WriteLn(ReadSettingKey('Section', 'Key2'));  > returns 'Something'
+//		When not found, returns a empty string.
+//		
+//	Needs updates for checking, validating data.
+//
+var
+	r: string;					// Return value of this function
+	sectionName: string;
+	inSection: boolean;
+	l: string;					// Line buffer
+	p: string;					// Path of the config file
+	conf: CTextFile;			// Class Text File 
 begin
-	bufferSize := SizeOf(bufferType);
-	//if MaxAvail < bufferSize then exit;  { Assure there is enough memory }
-	New (bufferPtr);              { Create the buffer }
-	fmSave := FileMode;           { Store the filemode }
-	FileMode := 0;                { To read also read-only files }
-	Assign (f1, fromFile);
-	{$I-} Reset (f1, 1); {$I+}    { Note the record size 1, important! }
-	if IOResult <> 0 then exit;   { Does the file exist? }
-	Assign (f2, toFile);
-	{$I-} Reset (f2, 1); {$I+}    { Don't copy on an existing file }
-	if IOResult = 0 then begin close (f2); exit; end;
-	{$I-} Rewrite (f2, 1); {$I+}  { Open the target }
-	if IOResult <> 0 then exit;
-	repeat                        { Do the copying }
-		BlockRead (f1, bufferPtr^, bufferSize, readCount);
-		{$I-} BlockWrite (f2, bufferPtr^, readCount, writeCount); {$I+}
-		if IOResult <> 0 then begin close (f1); exit; end;
-		until (readCount = 0) or (writeCount <> readCount);
-		writeln ('Copied ', fromFile, ' to ', toFile,' ', FileSize(f2), ' bytes');
-	close (f1); close (f2);
-	FileMode := fmSave;           { Restore the original filemode }
-	Dispose (bufferPtr);          { Release the buffer from the heap }
-end;  (* safecopy *)
+	p := GetProgramFolder() + '\' + CONF_NAME;
+	conf := CTextFile.Create(p);
+	conf.OpenFileRead();
+
+	r := '';
+	sectionName := '['+ section + ']';
+	inSection := false;
+	repeat
+		l := conf.ReadFromFile();
+		//WriteLn(inSection, #9, l);
+		
+		if Pos(sectionName, l) > 0 then
+		begin
+			//WriteLn('FOUND SECTION: ', sectionName);
+			inSection := true;
+		end;
+		
+		if inSection = true then
+		
+		begin
+			if (Pos(key, l) > 0) then
+			begin
+				//WriteLn('Found key ', key, ' found in section ', sectionName);
+				r := RightStr(l, Length(l) - Length(key + '='));
+				//WriteLn(r);
+				Break; // break the loop
+			end; // of if 
+		end; // of if inSection
+		
+	until conf.GetEof();
+	conf.CloseFile();
+	ReadSettingKey := r;
+end; // of function ReadSettingKey
+
 
 
 function FixLine(strLine: AnsiString): AnsiString;
@@ -127,226 +142,6 @@ begin
 	FixLine := strLine;
 end;
 	
-	
-function OccurrencesOfChar(const S: string; const C: char): integer;
-var
-	i: Integer;
-begin
-	result := 0;
-	for i := 1 to Length(S) do
-		if S[i] = C then
-			inc(result);
-end;
-
-//WriteEventLine(intEventPos, strFileLpr, intLineCount, strDcName, strLine);
-procedure WriteEventLine(intPosEvent: integer; strFileLpr: string; intLineNumber: integer; strDcName: string; strLine: AnsiString);
-var
-	strBuffer: AnsiString;
-	intHeaderCount: integer;
-	intFoundColumns: integer;
-begin
-	// Replace all pipes for a tab.
-	strBuffer := FixLine(strLine);
-	
-	// Add the prefix the DC name.
-	//strBuffer := strDcName + #9 + strBuffer;
-	// Build a new buffer with source file, line number, DC name and the original buffer.
-	strBuffer := strFileLpr + SEP + IntToStr(intLineNumber) + SEP + strDcName + SEP + strBuffer;
-	
-	//Writeln('WriteEventLine(): ', strBuffer);
-	// Get the number of columns from the header record.
-	intHeaderCount := arrEventFile[intPosEvent].HeaderCount;
-	
-	// Determine the number of columns in the line buffer. Add one of columns
-	// Finding 4 separators means 5 colums with information: c1|c2|c3|c4|c5
-	intFoundColumns := OccurrencesOfChar(strBuffer, SEP) + 1; 
-	
-	if intHeaderCount = intFoundColumns then
-	begin
-		WriteLn(arrEventFile[intPosEvent].FilePointer, strBuffer);
-		//Inc(gintLineCount);
-		Inc(arrEventFile[intPosEvent].count);
-		//Write('--Line: ', gintLineCount);
-	end;
-	{else
-		WriteLn('*** BAD LINE DETECTED: ', strBuffer);}
-end;
-
-
-
-function IsEventFound(intEventId: integer): integer;
-var
-	i: integer;
-	m: integer;
-	r: integer;
-begin
-	r := -1;
-	
-	// Get the number of items in the array.
-	m := Length(arrEventFile);
-	//WriteLn('IsEventFound(): m=', m);
-	if m = 0 then
-	begin
-		// array does not contain items.
-		//WriteLn('The array is empty!');
-		r := -1;
-	end
-	else
-	begin
-		for i := 0 to m do
-		begin
-			//WriteLn('IsEventFound():', i, #9, arrEventFile[i].EventId, #9, arrEventFile[i].Path);
-			if arrEventFile[i].EventId = intEventId then
-			begin
-				//WriteLn('FOUND IT ON POS ', i);
-				r := i;
-			end;
-		end;
-	end;
-	IsEventFound := r;
-end;
-
-
-
-procedure AddEventHeader(intEventId: integer; strHeader: AnsiString);
-var
-	intFound: integer;
-	arrHeader: TStringArray;
-	i: integer;
-begin
-	intFound := IsEventFound(intEventId);
-	WriteLn('AddEventHeader(): ', intEventId, '-', strHeader);
-	
-	SetLength(arrHeader, 0);
-	arrHeader := SplitString(strHeader, #9);
-	WriteLn('AddEventHeader(): number of columns=', Length(arrHeader));
-	for i := 0 to High(arrHeader) do
-	begin
-		WriteLn(intEventId, ': ', i, ' = ', arrHeader[i]);
-	end;
-	
-	arrEventFile[intFound].Header := strHeader;
-end;
-
-
-procedure AddEventHeaderFromFile(intEventId: integer);
-var
-	f: TextFile;
-	strFileName: string;
-	strLine: string;
-	arrLine: TStringArray;
-	i: integer;
-	strHeader: AnsiString;
-	intFound: integer;
-begin
-	WriteLn('AddEventHeaderFromFile(): ', intEventId);
-	strFileName := 'eebi-header.conf';
-	i := 0;
-	strHeader := '';
-	
-	AssignFile(f, strFileName);
-	{I+}
-	try 
-		Reset(f);
-		repeat
-			ReadLn(f, strLine);
-			//WriteLn(strLine, ' pos=', i);
-			SetLength(arrLine, 0);
-			arrLine := SplitString(strLine, TAB);
-			if StrToInt(arrLine[0]) = intEventId then
-			begin
-				//WriteLn('** Only the lines that start with ', intEventId);
-				// The first part of the line is an event number.
-				strHeader := strHeader + arrLine[1] + SEP;
-				Inc(i);
-			end;
-		until Eof(f);
-		CloseFile(f);
-		
-		intFound := IsEventFound(intEventId);
-		
-		WriteLn('ADD TO arrEventFile');
-		// Remove the last pipe char.
-		strHeader := LeftStr(strHeader, Length(strHeader) - 1);
-		// Update the header record with the header and the number of header items.
-		
-		arrEventFile[intFound].Header := strHeader;
-		arrEventFile[intFound].HeaderCount := i;
-		//WriteLn('intFound=', intFound);
-		//WriteLn('strHeader=', strHeader);
-		WriteLn('Number of header items = ', i, ' (i)');
-	except
-		on E: EInOutError do
-			WriteLn('ProcessLprFile(): file ', strFileName, ' handling error occurred, Details: ', E.ClassName, '/', E.Message);
-	end;
-end;
-
-
-
-procedure AddEventFile(intEventId: integer);
-var
-	i: integer;
-	strPath: string;
-begin
-	if IsEventFound(intEventId) = -1 then
-	begin
-		WriteLn('Found a new event id: ', intEventId);
-		
-		//WriteLn('LENGTH arrEventFile=', Length(arrEventFile)); // Init: 0
-		//WriteLn('  HIGH arrEventFile=', High(arrEventFile));   // Init: -1
-		
-		i := Length(arrEventFile);
-		SetLength(arrEventFile, i + 1);
-		arrEventFile[i].EventId := intEventId;
-		arrEventFile[i].count := 0;
-		strPath := GetProgramFolder() + '\events-' + IntToStr(intEventId) + '.psv';
-		arrEventFile[i].Path := strPath;
-		
-		AddEventHeaderFromFile(intEventId);
-		
-		{
-		if intEventId = 4625 then
-		begin
-			AddEventHeaderFromFile(4625);
-		end;
-		
-		if intEventId = 4770 then
-		begin
-			AddEventHeaderFromFile(4770);
-		end;
-		}
-		{if intEventId = 4770 then
-			AddEventHeader(4770, 'LprFile	LineNumber	DC	DateTime	EventId	EventStatus	AccountName	AccountDomain	ServiceName	ServiceId	TicketOptions	TicketEncryptionType	ClientAddress	ClientPort');
-		}
-		AssignFile(arrEventFile[i].FilePointer, strPath);
-		{I+}	
-		try 
-			if FileExists(strPath) = true then
-			begin
-				// Append to existing file.
-				Append(arrEventFile[i].FilePointer);
-			end
-			else
-			begin
-				// Create a new file.
-				WriteLn('New output file ', strPath, ' is created');
-				ReWrite(arrEventFile[i].FilePointer);
-				// Write the header, only when there is a header line in the Event record.
-				//WriteLn('len header=', Length(arrEventFile[i].Header));
-				if Length(arrEventFile[i].Header) > 0 then
-				begin
-					// When there is a header found, write it to the file.
-					WriteLn('Write a header line to event file: ', strPath);
-					WriteLn(arrEventFile[i].FilePointer, arrEventFile[i].Header);
-				end;
-			end;
-		except
-			on E: EInOutError do
-				WriteLn('ProcessLprFile(): file ', strPath, ' handling error occurred, Details: ', E.ClassName, '/', E.Message);
-		end;
-	end;
-end;
-
 
 function ExtractUniqueIdFromPath(p: string): string;
 //
@@ -371,7 +166,7 @@ begin
 end; // of function ExtractUniqueIdFromPath
 
 
-procedure ProcessLine(const e: integer; uid: string; lineNumber: integer; l: AnsiString);
+procedure ProcessLine(uid: string; lineNumber: integer; l: AnsiString);
 //
 //	Write a line to the output file
 //
@@ -387,19 +182,39 @@ begin
 end; // of procedure ProcessLine
 
 
-procedure ProcessLprFile(const e: integer; const p: string);
+procedure ProcessLprFile(const e: string; const p: string);
 var
 	lpr: CTextFile;
 	strLine: AnsiString;
 	intCurrentLine: integer;
 	uid: string;
+	header: Ansistring;
+	foundEvents: integer;
 begin
 	WriteLn('Extracting all ', e, ' events from file: ', p, ', please wait...');
-	csv := CTextFile.Create(IntToStr(e) + '.csv');
-	csv.OpenFileWrite();
 	
-	//2015-10-02 06:45:36|Security|NS00DC011.prod.ns.nl|4776|8|MICROSOFT_AUTHENTICATION_PACKAGE_V1_0|SVC_SP10_Setup|VM00AS1529|0x0
-	csv.WriteToFile('UID|LineNumber|DateTime|EventLog|Fqdn|EventId|EventType|AuthenticationPackage|Account|Host|LogonFailureCode');
+	foundEvents := 0;
+	
+	csv := CTextFile.Create(e + '.csv');
+	csv.OpenFileWrite();
+
+	if csv.AppendingToFile() = false then
+	begin
+		// Write the header because the file is new and does not have a header line yet.
+		//WriteLn(' >> Appending data to file: ', );
+	
+		// Read the header string from the .conf file.
+		header := ReadSettingKey(e, 'Header');
+		if Length(header) = 0 then
+		begin
+			WriteLn('*** Missing the header for event ', e, ' in the config file ', CONF_NAME , ' ***');
+			Exit;
+		end; // of if
+		// Write the lheader to the file
+		csv.WriteToFile(header);
+	end; // of if
+	
+	
 	
 	uid := ExtractUniqueIdFromPath(p);
 
@@ -409,180 +224,31 @@ begin
 		strLine := lpr.ReadFromFile();
 		intCurrentLine := lpr.GetCurrentLine();
 		//WriteLn(intCurrentLine:6, ': ', strLine);
+		WriteMod(intCurrentLine, STEP_MOD, 'lines');
 		
-		if Pos('|'  + IntToStr(e) + '|', strLine) > 0 then
+		if Pos('|'  + e + '|', strLine) > 0 then
 		begin
 			// The Event ID is found in the line
 			//WriteLn('>>> Event ID: ', e, ' is found in the line');
-			ProcessLine(e, uid, intCurrentLine, strLine);
-			WriteMod(intCurrentLine, STEP_MOD, 'lines')
+			ProcessLine(uid, intCurrentLine, strLine);
+			Inc(foundEvents);
 		end;
+		
 	until lpr.GetEof();
 	
 	lpr.CloseFile();
 	
-	WriteLn('Input file contained ', intCurrentLine, ' lines.');
+	WriteLn('Input file contained ', intCurrentLine, ' lines, extracted ', foundEvents, ' events.');
 	
 	
 	csv.CloseFile();
 end; // of procedure ProcessLprFile
 
 
-{
-procedure ProcessLprFile(strPathLpr: string);
-var
-	arrPath: TStringArray;
-	arrLine: TStringArray;
-	strLine: AnsiString;
-	strDcName: string;
-	//strEventId: string;
-	intEventId: integer;
-	//strPathTsv: string;
-	intLineCount: integer;
-	f: TextFile;
-	intEventPos: integer;
-	strFileLpr: string;
-begin
-	WriteLn('ProcessLprFile():');
-	WriteLn('  strPathLpr: ', strPathLpr);
-	
-	SetLength(arrPath, 0);
-	arrPath := SplitString(strPathLpr, '\');
-	strDcName := arrPath[Length(arrPath) - 2];
-	WriteLn('   strDcName: ', strDcName);
-	
-	strFileLpr := ExtractFileName(strPathLpr);
-	strFileLpr := StringReplace(strFileLpr, '.lpr', '', [rfReplaceAll, rfIgnoreCase]);
-	
-	
-	//strEventId := '4625';
-	
-	//strPathTsv := GetProgramFolder() + '\events-' + strEventId + '.tsv';
-	//WriteLn('  strPathTsv: ', strPathTsv);
-	
-	//AssignFile(gtfTsv, strPathTsv);
-	//ReWrite(gtfTsv); // Open file to write, create when needed.
-		
-	intLineCount := 0;
-	AssignFile(f, strPathLpr);
-	try 
-		Reset(f);
-		repeat
-			Inc(intLineCount);
-			ReadLn(f, strLine);
-			//WriteLn(strLine);
-			if intLineCount <> 1 then
-			begin
-				// Skip the header line!
-				SetLength(arrLine, 0);
-				arrLine := SplitString(strLine, '|');
-				// Get the event id for the line.
-				//strEventId := arrLine[1];
-				intEventId := StrToInt(arrLine[1]);
-				//if intEventId = 4625 then
-				//begin
-					AddEventFile(intEventId); // Add the event to the event array when it does not exist yet.
-					//WriteLn(strFileLpr, #9, intLineCount);
-					
-					intEventPos := IsEventFound(intEventId);
-					//WriteLn('--' + strPathLpr);
-					WriteEventLine(intEventPos, strFileLpr, intLineCount, strDcName, strLine);
-				//end;
-				SetLength(arrLine, 0);
-			end;
-		until Eof(f);
-		CloseFile(f);
-	except
-		on E: EInOutError do
-			WriteLn('ProcessLprFile(): file ', strPathLpr, ' handling error occurred, Details: ', E.ClassName, '/', E.Message);
-	end;	//ProcessLprFile(strDcName, strPathLpr, strPathTsv, gstrEventId);
-		
-	//CloseFile(gtfTsv);
-end; // 
-}
-
-{
-procedure FindFilesRecur(strFolderStart: string);
-var
-	sr: TSearchRec;
-	//strPath: string;
-	strFileSpec: string;
-	intValid: integer;
-	strFolderChild: string;
-	strPathFoundFile: string;
-begin
-	
-	//strPath := ExtractFilePath(strFolderStart); //keep track of the path ie: c:\folder\
-	strFileSpec := strFolderStart + '\*.*'; //keep track of the name or filter
-	WriteLn('FindFilesRecur(): ', strFolderStart);
-	
-	intValid := FindFirst(strFileSpec, faAnyFile, sr); //Find first file
-	//Writeln(intValid);
-	
-	while intValid = 0 do 
-	begin
-		if (sr.Name[1] <> '.') then
-		begin
-			if sr.Attr = faDirectory then
-			begin
-				WriteLn('Dir:    ', sr.Name);
-				strFolderChild := strFolderStart + '\' + sr.Name;
-				//WriteLn('strFolderChild=', strFolderChild);
-				FindFilesRecur(strFolderChild);
-			end
-			else
-			begin
-				strPathFoundFile := strFolderStart + '\' + sr.Name;
-				//WriteLn('strPathFoundFile:   ', strPathFoundFile);
-				ProcessLprFile(strPathFoundFile);
-			end;
-		end;
-		intValid := FindNext(sr);
-	end; // of while.
-end;
-}
-
-
-procedure ShowEventFile(strWhen: string);
-var	
-	i: integer;
-	m: integer;
-begin
-	WriteLn('-----------------------------------');
-	WriteLn('EVENTFILERECORDS: ', strWhen);
-	
-	m := High(arrEventFile);
-	//WriteLn('items in array: ', m + 1);
-	
-	if m < 0 then
-		WriteLn('No items in array arrEventFile!')
-	else
-	begin
-		for i := 0 to m do
-		begin
-			WriteLn(i, ':', #9, arrEventFile[i].EventId, #9, arrEventFile[i].Path, #9, arrEventFile[i].count);
-		end;
-	end;
-end;
-
-
-
-procedure CloseAllFiles();
-var
-	i: integer;
-begin
-	for i := 0 to High(arrEventFile) do
-	begin
-		WriteLn('Closing file: ', arrEventFile[i].Path);
-		CloseFile(arrEventFile[i].FilePointer);
-	end;
-end;
-
-
 procedure ProgUsage();
 begin
 	WriteLn('Usage:');
-	WriteLn('  ' + ParamStr(0) + ' <full path to file>');
+	WriteLn('  ' + ParamStr(0) + ' <event-id> <full path to file>');
 	WriteLn;
 	{WriteLn;
 	WriteLn('  --file <path>         Convert a single file.');
@@ -600,13 +266,15 @@ end; // of procedure ProgInit
 
 procedure ProgRun();
 begin
-	if ParamCount <> 1 then
+	if ParamCount <> 2 then
 		ProgUsage()
 	else
-		ProcessLprFile(4776, ParamStr(1));
+		// ParamStr(1): Event ID
+		// ParamStr(2): Path to LPR file
+		ProcessLprFile(ParamStr(1), ParamStr(2));
 end; // of procedure ProgRun
 
-
+{
 procedure ProgTest();
 var	
 	p: string;
@@ -617,9 +285,9 @@ begin
 	p := '\\10.4.222.20\TESTLPR\2015-10-02\NS00DC011\NS00DC011-Sec-20151002064535-h5e0AycC.lpr'; // Path of LPR file.
 	p := '\\10.4.222.20\TESTLPR\2015-10-02\NS00DC011\NS00DC011-Sec-20151002075035-tMrBYCL0.lpr'; // Path of LPR file.
 	//WriteLn(ExtractUniqueIdFromPath(p));
-	ProcessLprFile(e, p);
+	//ProcessLprFile(e, p);
 end;
-
+}
 
 procedure ProgDone();
 begin
